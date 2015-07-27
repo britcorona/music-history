82abce4131e09c1cf0b99af99eb710baa728a0b4
Brittney Corona <BrittneyLCorona@gmail.com>
2015-07-27 13:58:01 -0500

    JSON

---
+++ json.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+	$.ajax({
+		url: "songs.json"
+	}).done(function(data) {
+		var contentEl = $("#box2");
+
+		for (var i = 0; i <data.songs.length; i++) {
+		   var songName = data.songs[i].name;
+		   var songArtist = data.songs[i].artist;
+		   var songAlbum = data.songs[i].album;
+		   var songsText = '<h3 class="song-name">' + songName + '</h3>' + '<p class="song-name">' + " " + songArtist + " | " + songAlbum + "</p>";
+		   $("#box2").append(songsText);
+ 		}
+
+	})
+
+});
+
--- resources.js
+++ resources.js
@@ -1,20 +1,21 @@
-var songs = [];
-var element = document.getElementById("box2");
+// var songs = [];
+// var element = document.getElementById("box2");
 
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
 
-songs.push("Bad Blood > by Taylor Swift on the album 1989");
-songs.unshift("Faded > by Zhu on the album The Nightday");
+// songs.push("Bad Blood > by Taylor Swift on the album 1989");
+// songs.unshift("Faded > by Zhu on the album The Nightday");
 
-for ( i = 0; i < songs.length; i++) {
-	songs[i]=songs[i].replace(/>/g, '-');
-	songs[i]=songs[i].replace(/[*(@!]/g, '');
-	songs[i]=songs[i].replace('- by', '</h4><p>');
-	songs[i]=songs[i].replace('on the album', '|');
-	element.innerHTML += '<div class="song-name"><h4>' + songs[i] + ' | Genre' + '</p>' + '</div>';
-}
+// for ( i = 0; i < songs.length; i++) {
+// 	songs[i]=songs[i].replace(/>/g, '-');
+// 	songs[i]=songs[i].replace(/[*(@!]/g, '');
+// 	songs[i]=songs[i].replace('- by', '</h4><p>');
+// 	songs[i]=songs[i].replace('on the album', '|');
+// 	element.innerHTML += '<div class="song-name"><h4>' + songs[i] + ' | Genre' + '</p>' + '</div>';
+// }
+
