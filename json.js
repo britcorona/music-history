2acbdf04a304be1179cb3f148ed7670315f8bce0
Brittney Corona <BrittneyLCorona@gmail.com>
2015-07-29 09:09:12 -0500

    part 4

--- json.js
+++ json.js
@@ -10,10 +10,31 @@ $(document).ready(function() {
 		   var songArtist = data.songs[i].artist;
 		   var songAlbum = data.songs[i].album;
 		   var songsText = '<h3 class="song-name">' + songName + '</h3>' + '<p class="song-name">' + " " + songArtist + " | " + songAlbum + "</p>";
-		   $("#box2").append(songsText);
+		   $("#more").before(songsText);
  		}
-
 	})
 
+		$('#box2').click(function() {
+		  $(this).slideDown();
+		
+
+		$.ajax({
+			url: "secondsongs.json"
+			}).done(function(data){
+				var contentEl = $("#box2");
+
+			for (var i = 0; i <data.songs.length; i++) {
+			   var songName = data.songs[i].name;
+			   var songArtist = data.songs[i].artist;
+			   var songAlbum = data.songs[i].album;
+			   var songsText = '<h3 class="song-name">' + songName + '</h3>' + '<p class="song-name">' + " " + songArtist + " | " + songAlbum + "</p>";
+			   $("#more").before(songsText);
+			}
+		})
+	});	
 });
+
+
+
+	
 
